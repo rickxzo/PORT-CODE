0f75dd29e4b163d3707ba9e1269af63ae91e9f24
Gourav Jana <143085765+rickxzo@users.noreply.github.com>
2026-01-13 18:10:36 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     host: "0.0.0.0",
     port: process.env.PORT || 5173,
     allowedHosts: [
-      "port-code.onrender.com"
+      "rickxzo.onrender.com"
     ]
   },
   plugins: [vue()],
