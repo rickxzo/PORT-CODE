2fa9d1ab73f36fcbfb3d15768203c95cced3dbc0
Gourav Jana <143085765+rickxzo@users.noreply.github.com>
2026-02-08 21:00:09 +0530

    Update Landing.vue

--- src/views/Landing.vue
+++ src/views/Landing.vue
@@ -17,11 +17,33 @@
         <div>West Bengal, Ind AT&nbsp;&nbsp;{{ time }}</div>
       </div>
     </div>
+    <div class="absolute bottom-0 left-0 w-full px-12 pb-8">
+      <div class="flex gap-4 overflow-x-auto scrollbar-hide">
+        <div
+          v-for="(project, i) in projects"
+          :key="i"
+          class="min-w-[265px] h-[180px] relative bg-neutral-900 rounded-md overflow-hidden"
+        >
+          <img :src="project.image" class="w-full h-full object-cover opacity-90" />
+
+          <!-- INDEX -->
+          <span class="absolute -bottom-6 left-0 text-xs tracking-widest text-white/50">
+            [{{ String(i + 1).padStart(2, '0') }}]
+          </span>
+        </div>
+      </div>
+    </div>
   </section>
 </template>
 
 <script setup lang="ts">
 import { ref, onMounted, onUnmounted } from 'vue'
+
+const projects = ref([
+  { image: '/src/views/projects/aven.jpg' },
+  { image: '/src/views/projects/mad2.jpg' },
+  { image: '/src/views/projects/scpm.jpg' },
+])
 
 const time = ref('')
 
