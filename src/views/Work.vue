d4cd92e568611fdee7b79118891a64322e833e42
Gourav Jana <143085765+rickxzo@users.noreply.github.com>
2026-02-09 11:05:29 +0530

    Update Work.vue

--- src/views/Work.vue
+++ src/views/Work.vue
@@ -24,7 +24,7 @@
 </template>
 
 <script setup lang="ts">
-import { computed } from 'vue
+import { computed } from 'vue'
   
 const items = ref([
   { image: '/src/views/projects/aven.jpg', title: 'Aven C.S. Chatbot', date: '09/25' },
